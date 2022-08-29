b202ed9c2260851264137be67b3994aa66a843a3
renatoaldana <sergio.aldana@galileo.edu>
2022-08-28 18:23:04 -0600

    Finaliza validaciones

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -22,8 +22,10 @@ class RegisterController extends Controller
             'name' => 'required|max:30', //el nombre tiene que ser el mismo del atributo name
             'username' => 'required|unique:users|min:3|max:20',
             'email' => 'required|unique:users|email|max:60',
-            'password' => 'required'
+            'password' => 'required|confirmed|min:6'
         ]);
+
+        dd('Creando usuario');
 
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,8 +15,8 @@
                     <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
                         Nombre
                     </label>
-                    <input type="text" id="name" name="name" placeholder="Tu nombre" value="{{old('name')}}" class="border p-3 w-full rounded-lg
-                        @error('name') border-red-500 @enderror">
+                    <input type="text" id="name" name="name" placeholder="Tu nombre" class="border p-3 w-full rounded-lg
+                        @error('name') border-red-500 @enderror" value="{{old('name')}}">
                     @error('name')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                         {{$message}}
@@ -27,7 +27,8 @@
                     <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
                         Nombre de usuario
                     </label>
-                    <input type="text" id="username" name="username" placeholder="Nombre de usuario" class="border p-3 w-full rounded-lg">
+                    <input type="text" id="username" name="username" placeholder="Nombre de usuario" class="border p-3 w-full rounded-lg
+                    @error('username') border-red-500 @enderror" value="{{old('username')}}">
                 </div>
                 @error('username')
                 <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
@@ -38,7 +39,8 @@
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
                         Correo electrónico
                     </label>
-                    <input type="email" id="email" name="email" placeholder="Correo electrónico a registrar" class="border p-3 w-full rounded-lg">
+                    <input type="email" id="email" name="email" placeholder="Correo electrónico a registrar" class="border p-3 w-full rounded-lg
+                    @error('email') border-red-500 @enderror" value="{{old('email')}}">
                 </div>
                 @error('email')
                 <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
@@ -49,7 +51,8 @@
                     <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
                         Contraseña
                     </label>
-                    <input type="password" id="password" name="password" placeholder="Ingrese una contraseña" class="border p-3 w-full rounded-lg">
+                    <input type="password" id="password" name="password" placeholder="Ingrese una contraseña" class="border p-3 w-full rounded-lg
+                    @error('password') border-red-500 @enderror">
                 </div>
                 @error('password')
                 <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
